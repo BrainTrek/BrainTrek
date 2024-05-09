d9e67529d4779bb1b4f026783b88c1f3881264f0
prajnesh23 <svsp4598@gmail.com>
2024-05-10 00:17:27 +0530

    prajnesh2

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
-from flask import Flask,render_template, request, flash, url_for, redirect, session
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from authlib.integrations.flask_client import OAuth
 from flask_pymongo import PyMongo
-import pymongo
-from pymongo import MongoClient
 from bson.objectid import ObjectId
-from datetime import datetime
-import base64
+from pymongo import MongoClient
 import cohere
+from urllib.parse import urlencode
+import json
 
 app = Flask(__name__)
-app.secret_key = 'prajnesh@2310'
-# app.config['MONGO_URI'] = 'mongodb+srv://21pa1a12a5:Svsp9721@cluster0.nsdecj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-myclient = MongoClient("mongodb+srv://21pa1a12a5:Svsp9721@cluster0.nsdecj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# mongo = PyMongo(app)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
 
-# class User(UserMixin):
-#     def __init__(self, id, email):
-#         self.id = id
-#         self.email = email
+client = MongoClient("mongodb+srv://21pa1a12a5:Svsp9721@cluster0.n3mmdua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["BrainTrek"]
+users = db["users"]
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     user = mongo.db.users.find_one({'_id': user_id})
-#     if user:
-#         return User(id=user['_id'], email=user['email'])
-#     return None
+appConf = {
+    "OAUTH2_CLIENT_ID": "800948137796-11mkomvfo2gpqb55bl130td54j8sf2f6.apps.googleusercontent.com",
+    "OAUTH2_CLIENT_SECRET": "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",
+    "OAUTH2_META_URL": "https://accounts.google.com/.well-known/openid-configuration",
+    "FLASK_SECRET": "prajnesh@2310",
+    "FLASK_PORT": 5000
+}
+
+app.secret_key = appConf.get("FLASK_SECRET")
+
+# Google OAuth configuration
+oauth = OAuth(app)
+oauth.register("myApp",
+               client_id=appConf.get("OAUTH2_CLIENT_ID"),
+               client_secret=appConf.get("OAUTH2_CLIENT_SECRET"),
+               server_metadata_url=appConf.get("OAUTH2_META_URL"),
+               client_kwargs={
+                   "scope": "openid email profile",
+               }
+               )
 
 @app.route('/')
 def index():
     return render_template('lander.html')
-
-brainTrekdb = myclient["BrainTrek"]
-users = brainTrekdb["users"]
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -41,14 +45,13 @@ def signup():
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm-password']
-        print(name,email,password)
+
         if password == confirm_password:
-            new_user = {'name': name, 'email': email, 'password': password}
-            print(new_user)
-            insertion = users.insert_one(new_user)
+            insertion = users.insert_one({'name': name, 'email': email, 'password': password})
             flash('Account created successfully')
-            return redirect(url_for('signin'))
-        flash('Passwords do not match')
+            return redirect(url_for('index'))
+        else:
+            flash('Passwords do not match')
     return render_template('signup.html')
 
 @app.route('/signin', methods=['GET', 'POST'])
@@ -57,20 +60,33 @@ def signin():
         email = request.form['email']
         password = request.form['password']
         user = users.find_one({'email': email, 'password': password})
+
         if user:
-            # user_obj = User(id=user['_id'], email=user['email'])
-            # login_user(user_obj)
             return redirect(url_for('main'))
-        flash('Invalid email or password')
+        else:
+            flash('Invalid email or password')
     return render_template('signin.html')
 
+@app.route('/signin/google')
+def signin_google():
+    callback = url_for('authorized_google', next=request.args.get('next') or request.referrer or None, _external=True)
+    return oauth.myApp.authorize_redirect(redirect_uri=url_for("authorized_google", _external=True))
+
+@app.route('/authorized/google')
+def authorized_google():
+    # me = google.get('oauth2/v1/userinfo')
+    # user_email = me.data['email']
+    # Store user data in MongoDB or perform further actions
+    token = oauth.myApp.authorize_access_token()
+    session["user"] = token
+    return redirect(url_for('main'))
+
 @app.route('/main')
-# @login_required
 def main():
-    return render_template('main.html')
+    # Rest of your main route code here
+    return render_template('main.html', session=session.get("user"))
 
 @app.route('/quiz', methods=['GET', 'POST'])
-# @login_required
 def quiz():
     if request.method == 'POST':
         selected_option = request.form['quiz_option']
@@ -82,22 +98,25 @@ def quiz():
         # return redirect(url_for('quiz_result', quiz_id=mongo.db.quizzes.inserted_id))
     return render_template('quiz.html')
 
-# @app.route('/quiz/<quiz_id>')
-# @login_required
-# def quiz_result(quiz_id):
-#     quiz = mongo.db.quizzes.find_one({'_id': quiz_id})
-#     return render_template('quiz_result.html', quiz_data=quiz['quiz_data'])
+@app.route('/quiz/<quiz_id>')
+def quiz_result(quiz_id):
+    # quiz = mongo.db.quizzes.find_one({'_id': ObjectId(quiz_id)})
+    # if quiz:
+    #     return render_template('quiz_result.html', quiz_data=quiz['quiz_data'])
+    # else:
+    #     flash('Quiz not found')
+    #     return redirect(url_for('index'))
+    return "Quiz result page for quiz ID: " + quiz_id
 
 # @app.route('/history')
-# @login_required
 # def history():
 #     user_quizzes = mongo.db.quizzes.find({'user_id': current_user.id})
 #     return render_template('history.html', quizzes=user_quizzes)
 
 @app.route('/logout')
-# @login_required
 def logout():
-    # logout_user()
+    session.pop('user', None)
+    flash('You have been logged out.')
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
